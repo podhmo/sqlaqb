11806265b0cca0678ca6ea2d67bf83db9603581e
podhmo <podhmo+altair@beproud.jp>
2014-04-19 15:58:39 +0900

    suppress deprecation warling

--- sqlaqb/sqlaqb/tests/test_it_directly.py
+++ sqlaqb/sqlaqb/tests/test_it_directly.py
@@ -58,21 +58,21 @@ class Tests(unittest.TestCase):
 
     def test_missing(self):
         Base = declarative_base()
-        with self.assertRaisesRegexp(InvalidContract, "is empty"):
+        with self.assertRaisesRegex(InvalidContract, "is empty"):
             self._callFUT(Base, {})
 
     def test_single(self):
-        Base = declarative_base()        
+        Base = declarative_base()
         contract = {"Group": {"table_name": "groups"}}
         models = self._callFUT(Base, contract)
-        
+
         self.assertEqual(str(models.Group.__mapper__), "Mapper|Group|groups")
         self.assertTrue(issubclass(models.Group, Base))
         self.assertTrue(models.Group.name)
         self.assertTrue(models.Group.id)
 
     def test_single_with_mixin__has_field(self):
-        Base = declarative_base()        
+        Base = declarative_base()
         from datetime import datetime
 
         class TimeStampMixin(object):
@@ -81,67 +81,67 @@ class Tests(unittest.TestCase):
 
         contract = {"Group": {"table_name": "groups"}}
         models = self._callFUT(Base, contract, parents=[TimeStampMixin])
-        
+
         self.assertEqual(str(models.Group.__mapper__), "Mapper|Group|groups")
         self.assertTrue(models.Group.created_at)
         self.assertTrue(models.Group.updated_at)
 
     def test_same_base_twice__same(self):
-        Base = declarative_base()        
+        Base = declarative_base()
         contract = {"Group": {"table_name": "groups"}}
         models1 = self._callFUT(Base, contract)
-        models2 = self._callFUT(Base, contract)        
-        self.assertEquals(models1, models2)
+        models2 = self._callFUT(Base, contract)
+        self.assertEqual(models1, models2)
 
     def test_other_base_twice__different(self):
-        Base = declarative_base()        
-        Base2 = declarative_base()        
+        Base = declarative_base()
+        Base2 = declarative_base()
         contract = {"Group": {"table_name": "groups"}}
         models1 = self._callFUT(Base, contract)
-        models2 = self._callFUT(Base2, contract)        
-        self.assertNotEquals(models1, models2)
-        self.assertNotEquals(models1.Group, models2.Group)
+        models2 = self._callFUT(Base2, contract)
+        self.assertNotEqual(models1, models2)
+        self.assertNotEqual(models1.Group, models2.Group)
 
     def test_single__with_model_name(self):
-        Base = declarative_base()        
+        Base = declarative_base()
         contract = {"Group": {"table_name": "groups", "model_name": "MyGroup"}}
         models = self._callFUT(Base, contract)
-        
+
         self.assertEqual(str(models.MyGroup.__mapper__), "Mapper|MyGroup|groups")
         self.assertTrue(issubclass(models.MyGroup, Base))
-        
+
     def test_single__missing_depends(self):
-        Base = declarative_base()        
+        Base = declarative_base()
         contract = {"User": {"table_name": "users"}}
-        with self.assertRaisesRegexp(InvalidContract, "Group is missing"):
+        with self.assertRaisesRegex(InvalidContract, "Group is missing"):
             self._callFUT(Base, contract)
 
     def test_full(self):
-        Base = declarative_base()        
+        Base = declarative_base()
         contract = {"User": {"table_name": "users"}, 
                     "Group": {"table_name": "groups"}}
         models = self._callFUT(Base, contract)
-        
+
         self.assertEqual(str(models.Group.__mapper__), "Mapper|Group|groups")
         self.assertEqual(str(models.User.__mapper__), "Mapper|User|users")
         #xxx:
-        self.assertEquals(repr(list(models.User.group_id.foreign_keys)), 
+        self.assertEqual(repr(list(models.User.group_id.foreign_keys)), 
                           "[ForeignKey('groups.id')]")
 
     def test_full__with_table(self):
         metadata = sa.MetaData()
-        Base = declarative_base(metadata=metadata)        
+        Base = declarative_base(metadata=metadata)
         group = sa.Table("groups", metadata, 
-                         sa.Column("id", sa.Integer, primary_key=True, nullable=False),                          
+                         sa.Column("id", sa.Integer, primary_key=True, nullable=False),
                      )
         contract = {"User": {"table_name": "users"}, 
                     "Group": {"table": group}}
         models = self._callFUT(Base, contract)
-        
+
         self.assertEqual(str(models.Group.__mapper__), "Mapper|Group|groups")
         self.assertEqual(str(models.User.__mapper__), "Mapper|User|users")
         #xxx:
-        self.assertEquals(repr(list(models.User.group_id.foreign_keys)), 
+        self.assertEqual(repr(list(models.User.group_id.foreign_keys)), 
                           "[ForeignKey('groups.id')]")
 
 
@@ -155,11 +155,11 @@ class Tests(unittest.TestCase):
         contract = {"User": {"table_name": "users"}, 
                     "Group": {"model": MyGroup}}
         models = self._callFUT(Base, contract)
-        
+
         self.assertEqual(str(models.Group.__mapper__), "Mapper|MyGroup|my_groups")
         self.assertEqual(str(models.User.__mapper__), "Mapper|User|users")
         #xxx:
-        self.assertEquals(repr(list(models.User.group_id.foreign_keys)), 
+        self.assertEqual(repr(list(models.User.group_id.foreign_keys)), 
                           "[ForeignKey('my_groups.id')]")
 
 if __name__ == "__main__":
