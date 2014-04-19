bfa587fe88ed1c3a579f9ef1b3394ba5e668c7b6
podhmo <podhmo+altair@beproud.jp>
2014-04-19 16:17:05 +0900

    python3 support classifier

--- sqlaqb/setup.py
+++ sqlaqb/setup.py
@@ -8,6 +8,10 @@ setup(name='sqlaqb',
       description='utility for portable model definition of sqlalchemy models',
       long_description="", 
       author='podhmo',
+      classifiers=[
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 3'
+      ],
       package_dir={'': '.'},
       packages=find_packages('.'),
       install_requires = requires,
